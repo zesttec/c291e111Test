da1a16d2dc2864b245ca52819f39c97cd22fe39b
coco-pa <dfzhao95@gmail.com>
2024-01-11 20:15:26 -0500

    test0

---
+++ auto_push.sh
@@ -0,0 +1,10 @@
+# Add all changes to staging
+git add .
+
+# Commit changes with a message
+echo "Enter commit message:"
+read commit_message
+git commit -m "$commit_message"
+
+# Push changes to the main branch
+git push origin main
